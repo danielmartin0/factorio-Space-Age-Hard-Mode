403130192ca7c5b9d3dfa7bd2cf390ecd114e27a
danielmartin0 <github@danielgmartin.com>
2025-09-01 21:56:36 +0100

    nauvis split

--- Rocs-Hardcore-Nauvis/data-updates.lua
+++ Rocs-Hardcore-Nauvis/data-updates.lua
@@ -1,8 +1,7 @@
 --== Modules ==--
-local module_tiers = { "efficiency-module", "efficiency-module-2", "efficiency-module-3" }
-for _, module_name in pairs(module_tiers) do
-	if data.raw["module"][module_name] and data.raw["module"][module_name].effect.consumption then
-		data.raw["module"][module_name].effect.consumption = data.raw["module"][module_name].effect.consumption
+for _, module in pairs(data.raw["module"]) do
+	if module.category and module.category == "efficiency" and module.effect.consumption then
+		module.effect.consumption = module.effect.consumption
 			* settings.startup["hard-nauvis-efficiency-module-effectiveness-multiplier"].value
 	end
 end
